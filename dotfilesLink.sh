f6c156eb51ef6393d48774845528550f07d9f901
hiroya.shimamoto <shshimamo@gmail.com>
2018-11-10 19:01:54 +0900

    dotfilesLinkのリンクを修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 mkdir ~/.vim/ 
-ln -s ~/dotfiles/.vim/external_vimrc ~/.vim/external_vimrc
-ln -s ~/dotfiles/.vim/plugin ~/.vim/plugin
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.screenrc ~/.screenrc   
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf 
-ln -s ~/dotfiles/.vimperatorrc ~/.vimperatorrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc              
-ln -s ~/dotfiles/.vrapperrc ~/.vrapperrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc        
-ln -s ~/dotfiles/.xvimrc ~/.xvimrc       
-ln -s ~/dotfiles/.ideavimrc ~/.ideavimrc       
-ln -s ~/dotfiles/.gemrc ~/.gemrc      
-ln -s ~/dotfiles/.pryrc ~/.pryrc
+
+ln -s ~/src/github.com/shshimamo/dotfiles/.vim/external_vimrc ~/.vim/external_vimrc
+ln -s ~/src/github.com/shshimamo/dotfiles/.vim/plugin ~/.vim/plugin
+ln -s ~/src/github.com/shshimamo/dotfiles/.bashrc ~/.bashrc
+ln -s ~/src/github.com/shshimamo/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/src/github.com/shshimamo/dotfiles/.screenrc ~/.screenrc   
+ln -s ~/src/github.com/shshimamo/dotfiles/.tmux.conf ~/.tmux.conf 
+ln -s ~/src/github.com/shshimamo/dotfiles/.vimperatorrc ~/.vimperatorrc
+ln -s ~/src/github.com/shshimamo/dotfiles/.vimrc ~/.vimrc              
+ln -s ~/src/github.com/shshimamo/dotfiles/.vrapperrc ~/.vrapperrc
+ln -s ~/src/github.com/shshimamo/dotfiles/.zshrc ~/.zshrc        
+ln -s ~/src/github.com/shshimamo/dotfiles/.xvimrc ~/.xvimrc       
+ln -s ~/src/github.com/shshimamo/dotfiles/.ideavimrc ~/.ideavimrc       
+ln -s ~/src/github.com/shshimamo/dotfiles/.gemrc ~/.gemrc      
+ln -s ~/src/github.com/shshimamo/dotfiles/.pryrc ~/.pryrc
