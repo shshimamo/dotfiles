d0e912d3e70bf6e713fd96a42a4369ea2e50b8a2
hiroya.shimamoto <shshimamo@gmail.com>
2021-01-02 12:43:54 +0900

    Update rails-log-tail-tmux.sh

--- rails-log-tail-tmux.sh
+++ rails-log-tail-tmux.sh
@@ -2,7 +2,9 @@
 # tmux new-window -n name
 # tmux rename-window name
 tmux split-window -h
-tmux split-window -v -t 1
+tmux split-window -v -t 0
+tmux split-window -v -t 2
 tmux send-keys -t 0 'tail -f log/development.log' C-m
-tmux send-keys -t 1 'tail -f log/development.log | grep -E "UPDATE|INSERT|DELETE"' C-m
-tmux send-keys -t 2 'tail -f log/development.log | grep -E "GET|POST|PUT|PATCH|DELETE"' C-m
+tmux send-keys -t 1 'tail -f log/development.log | grep -E "GET|POST|PUT|PATCH|DELETE"' C-m
+tmux send-keys -t 2 'tail -f log/development.log | grep -E "SELECT"' C-m
+tmux send-keys -t 3 'tail -f log/development.log | grep -E "UPDATE|INSERT|DELETE"' C-m
