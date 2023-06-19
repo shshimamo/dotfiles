75c40d7601b286b9d1d7c23c7180762d208d84ea
hiroya.shimamoto <shshimamo@gmail.com>
2023-06-20 00:03:51 +0900

    update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,29 +1,24 @@
-if [ -z "$GOPATH" ]; then
-  echo 'GOPATHを指定してください(例: GOPATH=$HOME)'
-  exit 1
-fi
-
-path=$GOPATH
+ghqroot=$HOME/devel
 
 if [ ! -e ~/.vim/bundle ]; then
   # 存在しない場合
   mkdir -p ~/.vim/bundle 
 fi
 
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.vim/external_vimrc ~/.vim/external_vimrc
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.bashrc ~/.bashrc
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.gitconfig ~/.gitconfig
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.screenrc ~/.screenrc   
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.tmux.conf ~/.tmux.conf 
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.vimperatorrc ~/.vimperatorrc
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.vimrc ~/.vimrc              
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.vrapperrc ~/.vrapperrc
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.zshrc ~/.zshrc        
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.xvimrc ~/.xvimrc       
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.ideavimrc ~/.ideavimrc       
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.gemrc ~/.gemrc      
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.pryrc ~/.pryrc
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.ctags ~/.ctags
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.gitignore_global ~/.gitignore_global
-ln -ivs $path/src/github.com/shshimamo/dotfiles/.tabset ~/.tabset
-ln -ivs $path/src/github.com/shshimamo/dotfiles/rails-log-tail-tmux.sh ~/rails-log-tail-tmux.sh
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vim/external_vimrc ~/.vim/external_vimrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.bashrc ~/.bashrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gitconfig ~/.gitconfig
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.screenrc ~/.screenrc   
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tmux.conf ~/.tmux.conf 
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimperatorrc ~/.vimperatorrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimrc ~/.vimrc              
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vrapperrc ~/.vrapperrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.zshrc ~/.zshrc        
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.xvimrc ~/.xvimrc       
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ideavimrc ~/.ideavimrc       
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gemrc ~/.gemrc      
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.pryrc ~/.pryrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ctags ~/.ctags
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gitignore_global ~/.gitignore_global
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tabset ~/.tabset
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/rails-log-tail-tmux.sh ~/rails-log-tail-tmux.sh
