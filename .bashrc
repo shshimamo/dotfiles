461e35656a3ea435feb550e90962445eb4a73391
hiroya.shimamoto <shshimamo@gmail.com>
2023-08-31 03:08:50 +0900

    tidy

--- .bashrc
+++ .bashrc
@@ -1,21 +1,14 @@
-PS1='\[\033[36m\][\u@:\[\033[33m\]\w\[\033[36m\]]\[\033[0m\] \$ '
-alias ls='ls -G'
-alias ll='ls -laG'
+# PS1='\[\033[36m\][\u@:\[\033[33m\]\w\[\033[36m\]]\[\033[0m\] \$ '
 
 # for git
-function parse_git_branch {
-     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-function precmd() {
-     PROMPT="\h@\u:\W\$(parse_git_branch) \$ "
-}
-function proml {
-     #PS1="\h@\u:\W\$(parse_git_branch) \$ "
-      PS1='\[\033[36m\][\u@:\[\033[33m\]\W\[\033[36m\]]\[\033[0m\]$(parse_git_branch) \$ '
-}
-proml
-
-alias g='git'
-
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
+# function parse_git_branch {
+#      git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+# }
+# function precmd() {
+#      PROMPT="\h@\u:\W\$(parse_git_branch) \$ "
+# }
+# function proml {
+#      #PS1="\h@\u:\W\$(parse_git_branch) \$ "
+#       PS1='\[\033[36m\][\u@:\[\033[33m\]\W\[\033[36m\]]\[\033[0m\]$(parse_git_branch) \$ '
+# }
+# proml
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,24 +1,23 @@
 ghqroot=$HOME/devel
 
-if [ ! -e ~/.vim/bundle ]; then
+if [ ! -e ~/.aws/cli ]; then
   # 存在しない場合
-  mkdir -p ~/.vim/bundle 
+  mkdir -p ~/.aws/cli
 fi
 
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vim/external_vimrc ~/.vim/external_vimrc
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.bashrc ~/.bashrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.aws/cli/alias ~/.aws/cli/alias
 ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gitconfig ~/.gitconfig
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.screenrc ~/.screenrc   
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tmux.conf ~/.tmux.conf 
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimperatorrc ~/.vimperatorrc
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimrc ~/.vimrc              
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vrapperrc ~/.vrapperrc
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.zshrc ~/.zshrc        
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.xvimrc ~/.xvimrc       
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ideavimrc ~/.ideavimrc       
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gemrc ~/.gemrc      
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.screenrc ~/.screenrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimperatorrc ~/.vimperatorrc
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vimrc ~/.vimrc
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.vrapperrc ~/.vrapperrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.zshrc ~/.zshrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.xvimrc ~/.xvimrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ideavimrc ~/.ideavimrc
+ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gemrc ~/.gemrc
 ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.pryrc ~/.pryrc
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ctags ~/.ctags
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.ctags ~/.ctags
 ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.gitignore_global ~/.gitignore_global
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tabset ~/.tabset
-ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/rails-log-tail-tmux.sh ~/rails-log-tail-tmux.sh
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/.tabset ~/.tabset
+#ln -ivs $ghqroot/src/github.com/shshimamo/dotfiles/rails-log-tail-tmux.sh ~/rails-log-tail-tmux.sh
